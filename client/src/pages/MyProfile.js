ea0103bdbed808842f0721bfc43a8c9ac442aa36
unknown <mihajlo.c@elfak.rs>
2024-02-11 20:51:33 +0100

    Picture shown

--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -1,13 +1,14 @@
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
 import '../styles/MyProfile.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie'
 import Product from '../components/Product';
+import noImage from '../resources/blank.jpg';
 import { useParams } from 'react-router-dom';
 function MyProfile() {
 
-    const {userId} = useParams();
+    const { userId } = useParams();
     const [user, setUser] = useState(null);
     const [products, setPrducts] = useState(null);
     const [showReportDiv, setShowReportDiv] = useState(false);
@@ -15,87 +16,87 @@ function MyProfile() {
 
     useEffect(() => {
         const fetchUser = async () => {
-          try {
+            try {
 
-            const jwt = Cookies.get('jwt');
+                const jwt = Cookies.get('jwt');
 
-            // Create the request headers with the Authorization header containing the JWT token
-            const config = {
-                headers: {
-                    'Authorization': `Bearer ${jwt}` // Include the JWT token in the Authorization header
+                // Create the request headers with the Authorization header containing the JWT token
+                const config = {
+                    headers: {
+                        'Authorization': `Bearer ${jwt}` // Include the JWT token in the Authorization header
+                    }
+                };
+                const response = await axios.get(`http://localhost:5238/api/User/GetUserById?id=${userId}`, config); // Adjust the URL according to your backend route
+
+                // console.log(Cookies.get('jwt'))
+                // const response = await fetch(`http://localhost:5238/api/User/GetUserById?id=${userId}`, {
+                //   method: 'GET',
+                //   headers: {
+                //       'Content-Type': 'application/json',
+                //       'Authorization': 'Bearer ' + Cookies.get('jwt')
+                //   },
+                //   credentials: 'include' 
+                // });
+
+                console.log(response)
+                if (!response.data) {
+                    setUser(null);
+                } else {
+                    setUser(response.data);
                 }
-            };
-            const response = await axios.get(`http://localhost:5238/api/User/GetUserById?id=${userId}`, config); // Adjust the URL according to your backend route
-
-            // console.log(Cookies.get('jwt'))
-            // const response = await fetch(`http://localhost:5238/api/User/GetUserById?id=${userId}`, {
-            //   method: 'GET',
-            //   headers: {
-            //       'Content-Type': 'application/json',
-            //       'Authorization': 'Bearer ' + Cookies.get('jwt')
-            //   },
-            //   credentials: 'include' 
-            // });
-            
-            console.log(response)
-            if (!response.data) {
-              setUser(null);
-            } else {
-              setUser(response.data);
+            } catch (error) {
+                console.error('Error fetching user:', error);
             }
-          } catch (error) {
-            console.error('Error fetching user:', error);
-          }
         };
         fetchUser();
     }, [userId]);
 
 
-useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        //const response = await axios.get(`http://localhost:5238/api/Product/GetProductsByOwnerId?id=${userId}`); // Adjust the URL according to your backend route
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                //const response = await axios.get(`http://localhost:5238/api/Product/GetProductsByOwnerId?id=${userId}`); // Adjust the URL according to your backend route
 
-        const response = await fetch(`https://localhost:5238/api/Product/GetProductsByOwnerId?id=${userId}`, {
-            headers:{'Authorization': 'Bearer ' + Cookies.get('jwt')},
+                const response = await fetch(`https://localhost:5238/api/Product/GetProductsByOwnerId?id=${userId}`, {
+                    headers: { 'Authorization': 'Bearer ' + Cookies.get('jwt') },
+                    method: 'GET',
+                    credentials: 'include',
+                });
+
+                if (!response.data) {
+                    setPrducts(null);
+                } else {
+                    setPrducts(response.data);
+
+                }
+            } catch (error) {
+                console.error('Error fetching user:', error);
+            }
+        };
+        fetchProducts();
+    }, [products]);
+
+    const handleShowReportDiv = () => {
+        setShowReportDiv(!showReportDiv);
+    };
+
+    const handleReportUser = async () => {
+        setShowReportDiv(false);
+
+        const response = await fetch(`https://localhost:5238/api/UserReport/CreateUserReport?id=${userId}`, {
+            headers: { 'Authorization': 'Bearer ' + Cookies.get('jwt') },
             method: 'GET',
             credentials: 'include',
         });
-        
+
         if (!response.data) {
-          setPrducts(null);
+            setPrducts(null);
         } else {
-          setPrducts(response.data);
+            setPrducts(response.data);
 
         }
-      } catch (error) {
-        console.error('Error fetching user:', error);
-      }
-    };
-    fetchProducts();
-}, [products]);
-
-const handleShowReportDiv = () => {
-    setShowReportDiv(!showReportDiv);
-};
-
-const handleReportUser = async () => {
-    setShowReportDiv(false);
-
-    const response = await fetch(`https://localhost:5238/api/UserReport/CreateUserReport?id=${userId}`, {
-        headers:{'Authorization': 'Bearer ' + Cookies.get('jwt')},
-        method: 'GET',
-        credentials: 'include',
-    });
-    
-    if (!response.data) {
-      setPrducts(null);
-    } else {
-      setPrducts(response.data);
 
     }
-
-}
 
     return (
         <div className="gradient-custom-2" style={{ backgroundImage: 'linear-gradient(to top, rgba(25, 25, 112, 1), rgba(0, 150, 255, 0.6), rgba(25, 25, 112, 1))' }}>
@@ -108,8 +109,13 @@ const handleReportUser = async () => {
 
                                 </div>
                                 <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
-                                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
-                                        alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
+                                    <MDBCardImage
+                                        src={user && user.profilePicture ? `data:image/jpeg;base64,${user.profilePicture}` : noImage}
+                                        alt="Generic placeholder image"
+                                        className="mt-4 mb-2 img-thumbnail"
+                                        fluid
+                                        style={{ width: '150px', height:'150px', zIndex: '1' }}
+                                    />
                                 </div>
                                 <div className="ms-3" style={{ marginTop: '130px' }}>
                                     <MDBTypography tag="h5">Name: {user ? user.name : 'Loading...'}</MDBTypography>
@@ -123,14 +129,14 @@ const handleReportUser = async () => {
                             <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
                                 <div className="d-flex justify-content-end text-center py-1">
                                     <div>
-                                        <MDBCardText className="mb-1 h5">{user ? user.products.length: 'Loading...'}</MDBCardText>
+                                        <MDBCardText className="mb-1 h5">{user ? user.products.length : 'Loading...'}</MDBCardText>
                                         <MDBCardText className="small text-muted mb-0">Products added</MDBCardText>
                                     </div>
                                 </div>
                             </div>
                             {showReportDiv && (<div className='my-profile-report-div-content'>
                                 <label className='report-user-title'>Report user</label>
-                                <textarea  className="report-user-input" rows="3" placeholder='Report user...' type='text' required onChange={(e) => setReportUserContent(e.target.value)}/>
+                                <textarea className="report-user-input" rows="3" placeholder='Report user...' type='text' required onChange={(e) => setReportUserContent(e.target.value)} />
                                 <div className='report-user-button-div'>
                                     <label className='report-user-button' onClick={handleReportUser}>Report</label>
                                 </div>
